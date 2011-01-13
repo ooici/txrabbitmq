153fe49af6b6befb205c30f4cd5ed259f1daa9b3
Tim Freeman <tim@timfreeman.org>
2011-01-12 23:15:20 -0600

    Bumping to version 0.4 to account for two-way merge to new home @ github/ooici

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name = "txrabbitmq",
-  version="0.3",
+  version="0.4",
   description="RabbitMQ's 'rabbitmqctl' as a Twisted Service",
   
   author="Alex Clemesha",
