c67316d3d94a6b8fb4f773ee01e165b29d932a36
Tim Freeman <tim@timfreeman.org>
2011-05-16 14:14:34 -0500

    Updated txrabbitmq deps and version

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 
 setup(
   name = "txrabbitmq",
-  version="0.4",
+  version="0.5",
   description="RabbitMQ's 'rabbitmqctl' as a Twisted Service",
   
-  author="Alex Clemesha",
-  author_email="clemesha@ucsd.edu",
-  url="http://github.com/clemesha-ooi/txrabbitmq",
-  download_url="http://github.com/clemesha-ooi/txrabbitmq/tarball/master",
+  author="OOICI",
+  author_email="mmeisinger@ucsd.edu",
+  url="http://github.com/ooici/txrabbitmq",
+  download_url="http://ooici.net/releases",
   classifiers=[ ],
   packages=['txrabbitmq', 'twisted/plugins'],
   data_files=[('twisted/plugins', ['twisted/plugins/txrabbitmq_plugin.py'])],
-  install_requires = ['twotp', 'twisted', 'simplejson'],
+  install_requires = ['twotp==0.7', 'Twisted==10.2.0', 'simplejson==2.1.2'],
 )
